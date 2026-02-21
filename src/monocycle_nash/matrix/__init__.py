6008e3aad576f9c709e0b6ca1e01335ada77e764
pyran <121752109+pyran19@users.noreply.github.com>
2026-02-22 07:59:42 +0900

    Relax approximation type constraints to type hints only

--- src/monocycle_nash/matrix/__init__.py
+++ src/monocycle_nash/matrix/__init__.py
@@ -2,10 +2,22 @@ from .base import PayoffMatrix
 from .general import GeneralPayoffMatrix
 from .monocycle import MonocyclePayoffMatrix
 from .builder import PayoffMatrixBuilder
+from .approximation import (
+    PayoffMatrixApproximation,
+    MonocycleToGeneralApproximation,
+    PayoffMatrixDistance,
+    MaxElementDifferenceDistance,
+    ApproximationQualityEvaluator,
+)
 
 __all__ = [
     "PayoffMatrix",
     "GeneralPayoffMatrix",
     "MonocyclePayoffMatrix",
     "PayoffMatrixBuilder",
+    "PayoffMatrixApproximation",
+    "MonocycleToGeneralApproximation",
+    "PayoffMatrixDistance",
+    "MaxElementDifferenceDistance",
+    "ApproximationQualityEvaluator",
 ]
